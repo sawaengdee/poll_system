f1f3d751e5f0abfdcc4dead3594d503f989e2767
sawaengdee <110087157+sawaengdee@users.noreply.github.com>
2022-11-18 08:58:51 +0700

    new

--- check_register.php
+++ check_register.php
@@ -19,8 +19,8 @@
             echo '<script>  alert("มีข้อมูลผู้ใช้นี้แล้ว");   </script>';
             header("refresh:0.2; url=register.html");
         }elseif($password_1 == $password_2){
-            $sql = "INSERT INTO member (username, password, fname, lname, email)
-            VALUE ('$username', '$password_1', '$fname', '$lname', '$email');";
+            $sql = "INSERT INTO member (username, password, fname, lname, email,id_role)
+            VALUE ('$username', '$password_1', '$fname', '$lname', '$email', 'r02');";
             if ($conn->multi_query($sql) === true){
                     echo '<script>  alert("กรอกข้อมูลสำเร็จ");   </script>';
                     header("refresh:0.2; url=login.html");
